f8ea49ebab12aa82e4185a66df1d92f917bda222
duyquang422 <duy_quang422@yahoo.com>
2016-04-28 23:48:02 +0700

    delete landing page

--- module/LandingPage/autoload_classmap.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	'LandingPage\Controller\IndexController'	=>	__DIR__ . '/src/Controller/IndexController.php',
-    'LandingPage\Controller\PostsController'	=>	__DIR__ . '/src/Controller/PostsController.php',
-    'LandingPage\Controller\CategoryPostsController'	=>	__DIR__ . '/src/Controller/CategoryPostsController.php'
-);
